33f5093675dad5f8472541d354aa1f65bdf3fac0
Wojciech <wojciechrozwalka@gmail.com>
2021-01-02 21:56:35 +0100

    add success register template

--- src/main/java/javer/springbootregistrationlogin/controller/UserController.java
+++ src/main/java/javer/springbootregistrationlogin/controller/UserController.java
@@ -1,12 +1,19 @@
 package javer.springbootregistrationlogin.controller;
 
 import javer.springbootregistrationlogin.model.User;
+import javer.springbootregistrationlogin.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
 
     @GetMapping("")
     public String viewHomePage() {
@@ -17,5 +24,11 @@ public class UserController {
     public String showSignUpForm(Model model) {
         model.addAttribute("user", new User());
         return "sign_up_form";
+    }
+
+    @PostMapping("/process_register")
+    public String processRegistration(User user) {
+        userRepository.save(user);
+        return "register_success";
     }
 }
