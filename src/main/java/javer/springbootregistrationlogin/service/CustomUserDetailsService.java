0793d8109eeeb5e5f46194684b5d7c7b3d631896
Wojciech <wojciechrozwalka@gmail.com>
2021-01-03 17:27:57 +0100

    clean code and refactor

--- src/main/java/javer/springbootregistrationlogin/service/CustomUserDetailsService.java
+++ src/main/java/javer/springbootregistrationlogin/service/CustomUserDetailsService.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CustomUserDetailsService implements UserDetailsService {
 
     @Autowired
@@ -15,7 +17,7 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(email);
+        User user = userRepository.findUserByEmail(email);
         if (user == null) {
             throw new UsernameNotFoundException("User not found");
         }
